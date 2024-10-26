aea8e508a0dc27d9a7468a70796b45108b1e569f
Leonor Sinogas <leonor.sinogas@gmail.com>
2024-10-26 12:28:41 -0400

    app intro screen

---
+++ appHello/app_settings.py
@@ -0,0 +1,13 @@
+import pygame
+import math
+
+
+SCREEN_SIZE = 800
+
+blue = (0, 0, 225)
+white = (255, 255, 255)
+gold = (249, 216, 73)
+
+light_blue = (151, 204, 232)
+
+screen = pygame.display.set_mode((800, 800))
---
+++ appHello/helloworldapp.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+from introscreen import *
+
+
+def main():
+    pygame.init()
+    show_intro_screen()
+
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
