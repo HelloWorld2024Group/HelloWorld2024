c822674e824e33cde5d46e963e0afe3bf3ca2028
Leonor Sinogas <leonor.sinogas@gmail.com>
2024-10-27 02:35:57 -0400

    project name intro screen

--- appHello/introscreen.py
+++ appHello/introscreen.py
@@ -33,9 +33,16 @@ def show_intro_screen():
         screen.blit(text, text_rect)
 
 
-        start_font = pygame.font.SysFont(None, 30)
+        start_font = pygame.font.SysFont(None, 50)
         start_text = start_font.render("Start", True, white) # color letters
         start_text_rect = start_text.get_rect(center=start_button.center)
         screen.blit(start_text,start_text_rect)
+
+        title_font = pygame.font.SysFont(None, 100)
+        title_text = title_font.render("NEWSFLASH AI", True, white) # color letters
+        #title_text_rect = start_text.get_rect(center - 20 =start_button.center)
+        screen.blit(title_text,(130, 250))
+
+
 
         pygame.display.flip()
