0173f82efcc85b898f29c836717b57bc515db226
laylashihab <lshihab@purdue.edu>
2024-10-27 02:28:29 -0400

    made json a function

--- jsonwriter.py
+++ jsonwriter.py
@@ -3,10 +3,14 @@ import articles_to_output
 
 feed_url = 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'
 
-#Data to be written into the JSON file (usually a dictionary or list)
-data = articles_to_output.summarizes_articles(feed_url)
+def json_writer(feed_url):
 
-# Writing data into a JSON file
-with open('data.json', 'w') as file:
-    json.dump(data, file, indent=4)  # indent=4 adds pretty formatting (optional)
+    #Data to be written into the JSON file (usually a dictionary or list)
+    data = articles_to_output.summarizes_articles(feed_url)
 
+    # Writing data into a JSON file
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4)  # indent=4 adds pretty formatting (optional)
+
+
+json_writer(feed_url=feed_url)
